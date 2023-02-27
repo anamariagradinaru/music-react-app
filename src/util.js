b6f54188d6d361a5b7b7ac707c96b6a98ce72e64
Ana-Maria Grădinaru <anamariagradinaru92@yahoo.com>
2023-02-27 21:30:44 +0200

    Made components

---
+++ src/util.js
@@ -0,0 +1,40 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export const songlist = [
+    {
+        name: 'Dreaming',
+        artist: 'Misha, Jussi Halme, El Train, goodie bags',
+        cover: 'https://i.scdn.co/image/ab67616d0000b273194aaa78f555a7ccac3e0abf',
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=48087',
+        id: uuidv4(),
+        color: ['#BA4a46', '#FDF0DD'],
+        active: true,
+    },
+    {
+        name: 'Early Rise',
+        artist: 'illiterate, Mr Sliptz',
+        cover: 'https://i.scdn.co/image/ab67616d0000b273194aaa78f555a7ccac3e0abf',
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=48088',
+        id: uuidv4(),
+        color: ['#BA4a46', '#FDF0DD'],
+        active: false,
+    },
+    {
+        name: 'Another Day',
+        artist: 'illiterate, Mr Sliptz',
+        cover: 'https://i.scdn.co/image/ab67616d0000b273194aaa78f555a7ccac3e0abf',
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=48089',
+        id: uuidv4(),
+        color: ['#BA4a46', '#FDF0DD'],
+        active: false,
+    },
+    {
+        name: 'Salt Lamp',
+        artist: 'illiterate, Mr Sliptz',
+        cover: 'https://i.scdn.co/image/ab67616d0000b273194aaa78f555a7ccac3e0abf',
+        audio: 'https://mp3.chillhop.com/serve.php/?mp3=48090',
+        id: uuidv4(),
+        color: ['#BA4a46', '#FDF0DD'],
+        active: false,
+    },
+];
